4d55e1cf98d280529aa1cc544f8a1830e41911da
killuaJ2002 <kjcodes2002@gmail.com>
2025-08-04 18:06:41 +0530

    added user in response of login controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -56,7 +56,14 @@ const loginUser = async (req, res) => {
     if (!passwordMatch)
       return res.status(401).json({ message: "Invalid credentials" });
     const token = generateToken(user.id);
-    res.status(200).json({ message: "Login succesfull", token });
+    res.status(200).json({
+      message: "Login succesfull",
+      token,
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+    });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Failed to log in" });
